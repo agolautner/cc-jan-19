0c903a3447bcf9cf6d6270e0750d33261b1212d9
agolautner <lautner.agoston@gmail.com>
2022-01-19 13:54:33 +0100

    axios request made

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('hello world')
+
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('http://www.nemzetisport.hu')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    console.log('status: ', response.status);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
